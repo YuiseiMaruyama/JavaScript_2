b9ca19ff50a4fff8b8bdef2c86e8b8981b1007c2
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-03-31 16:08:55 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -2,24 +2,25 @@
 
 //関数
 
-function showAd(){
+function showAd(message = 'AD'){ //仮引数
     console.log('--------');
-    console.log('---AD---');
+    console.log(`---${message}---`);
     console.log('--------');
 
 }
 
-showAd();
+showAd('Header AD'); //実引数
 
 
 console.log('Tom is great');
 console.log('Bob is great');
 
+// showAd('AD');
 showAd();
 
 console.log('Steve is great');
 console.log('Rich is great');
 
-showAd();
+showAd('Footer AD');
 
 
