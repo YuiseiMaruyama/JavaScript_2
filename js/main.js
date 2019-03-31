dfdc51fc281d749d409016fdfff9f3ff9c0e9fe9
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-03-31 15:46:36 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,31 +1,9 @@
 'use strict';
 
-const signal = 'pink';
-
-// if(signal=== 'red'){
-//     console.log('stop');
-// }else if(signal=== 'yallow'){
-//     console.log('causion');
-// }else if(signal=== 'blue'){
-//     console.log('go');
-// }
-
-switch(signal){
-    case 'red':
-    console.log('stop');
-    break;
-
-    case 'yellow':
-    console.log('causion');
-    break;
-
-    case 'blue':
-    case'green':
-    console.log('go');
-    break;
-    default:
-    console.log('wrong signal');
-    break;
+for(let i =1; i <= 10; i++){
+    // console.log('hello');
+    // console.log(i + 'hello');
+    console.log(`${i} hello`);
 }
 
 
