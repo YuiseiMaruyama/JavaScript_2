e041d0e644c5abab735d857a8d0d03dd53c69238
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-03-31 16:03:41 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 'use strict';
 
-for(let i = 1; i <=10; i++){
-    // if(i===4){
-    //     if(i % 3 === 0){
-    //     continue;//iを3で割ったときのcontinue以降の処理を実行しない
-    // }
-    if(i === 4){
-        break;//i === 3 までの処理で終了
-    }
-    console.log(i);
+//関数
+
+function showAd(){
+    console.log('--------');
+    console.log('---AD---');
+    console.log('--------');
+
 }
+
+showAd();
+
+
+console.log('Tom is great');
+console.log('Bob is great');
+
+showAd();
+
+console.log('Steve is great');
+console.log('Rich is great');
+
+showAd();
+
+
