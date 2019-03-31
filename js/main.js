17aef8042c22731eaecbc366bdae4be107f6ea66
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-03-31 15:58:44 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
-//while
-
-// let hp = 200;
-
-// while(hp > 0){
-//     console.log(`${hp} HP left!`);
-//     hp -= 15;
-// }
-
-for(let hp =200; hp >0; hp -=15){
-    console.log(`${hp} HP left!`);
+for(let i = 1; i <=10; i++){
+    // if(i===4){
+    //     if(i % 3 === 0){
+    //     continue;//iを3で割ったときのcontinue以降の処理を実行しない
+    // }
+    if(i === 4){
+        break;//i === 3 までの処理で終了
+    }
+    console.log(i);
 }
-
