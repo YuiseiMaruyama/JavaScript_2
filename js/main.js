b18127000347488fef1ec70562b3c907f34a6f7a
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-01 17:36:46 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -8,11 +8,21 @@
     // const player = ['taguchi',32];
 
     const player = {
+        //プロパティ
         name: 'taguchi',
         score: '32',
     };
 
-    console.log(player);
+    // console.log(player.name);
+    // console.log(player['name']);
+
+    // player.score = 100;
+
+    // player.email = 'taguchi@gmail.com';
+    // console.log(player);
+
+    delete player.score;
+    console.log(player); 
 }
 
 
