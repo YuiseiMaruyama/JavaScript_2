ad77405ad5b868062939466b50eb8a95ce56a224
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-02 02:03:42 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -3,40 +3,17 @@
 // Scope
 
 {
-    //クラス
+//    let x = 1;
+//    let y = x;
+//    x = 5;
+//    console.log(x); //5
+//    console.log(y); //1
 
-    
-
-   class Player{ //親クラス
-       constructor(name,score){ //メソッド
-        this.name = name;
-        this.score = score;
-       }
-
-       showInfo(){//メソッド
-           console.log(`name:${this.name} score:${this.score}`);
-       }
-
-       static showVersion(){//メソッド
-           console.log('Player class ver. 1.0');
-       }
-   }
-
-   class SoccerPlayer extends Player{ //子クラス
-    constructor(name, score, number){
-        super(name,score); //親クラスのconstructorを呼び出す
-        this.number = number;
-    }
-    kick(){
-        console.log('Gooooaal');
-    }
-   }
-
-   const tsubasa = new SoccerPlayer('tsubasa',99,10);
-   
-   tsubasa.kick();
-   console.log(tsubasa.number);
-   tsubasa.showInfo();
+   let x = [1,2];
+   let y = x;
+   x[0] = 5;
+   console.log(x); //[5,2]
+   console.log(y); //[1,2]だが実際は[5,2] なぜなら　xの値ではなく値への参照がコピーされる
 }
 
 
