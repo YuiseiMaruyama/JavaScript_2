4c10b1bc69c47a787b7b0dbdfdf4b7296da04761
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-01 18:12:14 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -3,26 +3,33 @@
 // Scope
 
 {
-    //オブジェクト
+    //クラス
 
-    // const player = ['taguchi',32];
+    
 
-    const player = {
-        //プロパティ
-        name: 'taguchi',
-        score: '32',
-    };
+   class Player{
+       constructor(name,score){ //メソッド
+        this.name = name;
+        this.score = score;
+       }
 
-    // console.log(player.name);
-    // console.log(player['name']);
+       showInfo(){//メソッド
+           console.log(`name:${this.name} score:${this.score}`);
+       }
 
-    // player.score = 100;
+       static showVersion(){//メソッド
+           console.log('Player class ver 1.0');
+       }
+   }
 
-    // player.email = 'taguchi@gmail.com';
-    // console.log(player);
+   const taguchi = new Player('taguchi',32);
+   const fkoji = new Player('fkoji',44);
 
-    delete player.score;
-    console.log(player); 
+//    console.log(taguchi);
+taguchi.showInfo();
+fkoji.showInfo();
+
+Player.showVersion();
 }
 
 
