c92603c16f8ebf2979b25b0f0b6039bb234a3936
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-01 22:22:50 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 
     
 
-   class Player{
+   class Player{ //親クラス
        constructor(name,score){ //メソッド
         this.name = name;
         this.score = score;
@@ -18,18 +18,25 @@
        }
 
        static showVersion(){//メソッド
-           console.log('Player class ver 1.0');
+           console.log('Player class ver. 1.0');
        }
    }
 
-   const taguchi = new Player('taguchi',32);
-   const fkoji = new Player('fkoji',44);
+   class SoccerPlayer extends Player{ //子クラス
+    constructor(name, score, number){
+        super(name,score); //親クラスのconstructorを呼び出す
+        this.number = number;
+    }
+    kick(){
+        console.log('Gooooaal');
+    }
+   }
 
-//    console.log(taguchi);
-taguchi.showInfo();
-fkoji.showInfo();
-
-Player.showVersion();
+   const tsubasa = new SoccerPlayer('tsubasa',99,10);
+   
+   tsubasa.kick();
+   console.log(tsubasa.number);
+   tsubasa.showInfo();
 }
 
 
