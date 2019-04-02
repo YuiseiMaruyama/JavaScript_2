636fbf81ad66ca48bbf3fe4659979c326afc0a2e
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-02 20:14:48 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
-//  #17 Mathオブジェクトを使ってみよう
+//  #18 Dateオブジェクトを使ってみよう
 
 {
-    //Math
-    console.log(Math.PI);
-    console.log(Math.floor(Math.random()*6+1));//0-0.999....まで
-    
+    //Date
+    const d = new Date();
+
+    // console.log(d);
+    // console.log(d.getFullYear());
+    // console.log(d.getMonth());//0-11
+    // console.log(d.getDate());//日にち
+    // console.log(d.getDay());//曜日で0-6で表現
+    // console.log(d.getHours());
+    // console.log(d.getMinutes());
+    // console.log(d.getSeconds());
+    // console.log(d.getMilliseconds());
+
+    console.log(d.getTime()); //UTF 1970/01/01 00:00:00
 }
