3ae76d2216a4534427274e6112c52c6b59942cfb
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-02 18:26:17 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 'use strict';
 
-//  #14 スプレッド演算子を使ってみよう
+//  #15 分割代入を使ってみよう
 
 {
-    //Object
+    // const numbers = [1,2];
+    // // const a = numbers[0];
+    // // const b = numbers[1];
+    // const [a,b] = numbers;
+    // console.log(a); 
+    // console.log(b); 
 
-    // const a = [10,20];
-    // const b = [1,2,...a];//aのindex(10,20)がbで展開される
-    // console.log(b);
-   
-    // a[0] = 100;
-    // console.log(b);
+    // const numbers = [1,2,3,8];
+    // const [a,b,...rest] = numbers;
+    // console.log(a); //1
+    // console.log(b); //2
+    // console.log(rest); //[3, 8]
 
-    // const a = [10,20];
-    // const sum = (a,b) => a + b;
-    // console.log(sum); //(a,b) => a + b
-    // console.log(sum(...a)); //30
-
-    const o1 = {a:1};
-    const o2 = {...o1,b:2};
-    console.log(o2); //{a: 1, b: 2}
+    const player = {
+        name:'taguchi',
+        score: 55,
+        hp: 33,
+        mp: 22,
+    };
+    const {name, score, ...points} = player;
+    console.log(name);
+    console.log(score);
+    console.log(points);
 }
