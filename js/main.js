27ab70731570ae95bdf9d3cec9ecaea12e9224ff
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-03 03:57:42 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
 'use strict';
 
-//   #10 要素のコピー､削除をしてみよう
+//   #13 イベントリスナーを使ってみよう
 
 {
-    // console.log(document.querySelectorAll('input[type="checkbox"]')[0].checked);//false
-    // console.log(document.querySelectorAll('input[type="checkbox"]')[1].checked);//true
+    const button = document.querySelector('button');
 
-    // document.querySelectorAll('input[type="checkbox"]')[0].checked = true; //macOSの方にもチェック
-
-    // console.log(document.querySelectorAll('input[type="radio"]')[0].checked);//true
-    // console.log(document.querySelectorAll('input[type="radio"]')[1].checked);//false
-
-    // document.querySelectorAll('input[type="radio"]')[1].checked = true;
-
-    console.log(document.querySelectorAll('select > option')[0].selected);
-    console.log(document.querySelectorAll('select > option')[1].selected);
-    console.log(document.querySelectorAll('select > option')[2].selected);
-
-    document.querySelectorAll('select > option')[2].selected = true; //item2が選択される
+    button.addEventListener('dblclick', () =>{
+        console.log('clicked');
+    });
 
 }
