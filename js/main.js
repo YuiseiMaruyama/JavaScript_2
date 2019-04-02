d61d2614d9304ef8dd618b1efd015cf21311b79a
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-03 03:09:06 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
-//   #07 クラス属性を操作してみよう
+//   #08 classListを使ってみよう
 
 {
    const div = document.querySelector('div');
 
 //    div.className = 'border-pink';//HTMLのdivのclass名がbox -> border-pinkに書き換わってしまう
-   div.className = ' box border-pink'; // boxクラスも書いておく
+//    div.className = ' box border-pink'; // boxクラスも書いておく
+        // div.classList.add ('border-pink');
+        // div.classList.remove ('box');
+
+        // if(div.classList.contains ('border-pink')){
+        //     div.classList.remove ('border-pink');
+        // }else{
+        //     div.classList.add ('border-pink');
+        // }
+
+        //toggleはdivにborder-pinkが存在なし->追加、存在する->削除
+        div.classList.toggle('border-pink');
 
 }
