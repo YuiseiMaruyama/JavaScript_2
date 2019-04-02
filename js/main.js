d3658c85818a9284c66eea8df707e9dbf5875260
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-02 20:01:58 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,29 +1,12 @@
 'use strict';
 
-//  #15 分割代入を使ってみよう
+//  #16 文字列を操作してみよう
 
 {
-    // const numbers = [1,2];
-    // // const a = numbers[0];
-    // // const b = numbers[1];
-    // const [a,b] = numbers;
-    // console.log(a); 
-    // console.log(b); 
-
-    // const numbers = [1,2,3,8];
-    // const [a,b,...rest] = numbers;
-    // console.log(a); //1
-    // console.log(b); //2
-    // console.log(rest); //[3, 8]
-
-    const player = {
-        name:'taguchi',
-        score: 55,
-        hp: 33,
-        mp: 22,
-    };
-    const {name, score, ...points} = player;
-    console.log(name);
-    console.log(score);
-    console.log(points);
+    
+    const str = 'hello';
+    // console.log(str.length);//5
+    // console.log(str.substring(2,4));//(start,end) // ll
+    console.log(str[1]);//e
+    
 }
