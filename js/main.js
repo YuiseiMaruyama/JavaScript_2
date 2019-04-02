d694056fb7d5b4fea59958bed508b75dd14df03a
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-03 00:46:49 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
-//  #02 要素を指定してみよう
+//  #03 querySelectorを使ってみよう
 
 {
-    document.getElementById('target').textContent = 'Changed!';
+    document.querySelector('h1').textContent = 'H1!';
+
+    // nth-child(odd) 奇数番目の要素指定
+    document.querySelectorAll('li:nth-child(odd)').forEach(li => { // HTML内のliを全て適用
+        li.textContent = 'li!'
+    });
 }
