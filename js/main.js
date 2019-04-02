967c62e972a4cd8d9160c13a444dcd6a8c94b437
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-02 16:33:49 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 'use strict';
 
-// #09 配列の要素を操作してみよう
+//  #10 spliceで配列を変更してみよう
 
 {
-    const a = [1,5,9];
+    const a = [1,5,10];
 
-    //add
-    // a.unshift(100); //先頭に100追加
-    // a.push(200,300); //後ろにに200、300追加
+    a.splice(2, 0, 6,7);// (配列を変化させる要素の添字,削除する数,追加する要素)
+    console.log(a);//[1, 5, 6, 7, 10]
 
-    // console.log(a);
-
-    //remove
-    a.shift(); //先頭から1を削除
-    a.pop();   //後ろから9を削除
-    console.log(a);//[5]
+    const removed = a.splice(1, 2);// (配列を変化させる要素の添字,削除する数,追加する要素)
+    console.log(a);//[1, 7, 10]
+    console.log(removed);//5,6
     
 }
