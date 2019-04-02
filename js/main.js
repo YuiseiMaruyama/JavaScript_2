7f6421f84d282bb3ff9d61f4b217fa80189969bb
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-02 20:39:20 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
-//  #20 alert､confirmを使ってみよう
-
+//  #21 setIntervalで繰り返し処理をしよう
 {
-    // window.alert('hello');
-    // alert('hello');
+    let i = 0;
 
-    const answer = confirm('Are you ready?'); // okとキャンセルがつく
-    console.log(answer);
+
+    const showTime = () =>{
+        console.log(new Date());
+        i++;
+        if(i > 2){
+            clearInterval(timerId);
+        }
+    };
+    // setInterval(showTime,ミリ秒); // showTime()だと関数式showTimeを実行した結果をsetIntervalに代入することになる
+
+
+    let timerId =setInterval(showTime,1000);
 }
