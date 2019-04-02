0de57c5280c07e317b46b22424022a05ebc4cf61
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-03 03:17:00 +0900

    add new file***

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 'use strict';
 
-//   #08 classListを使ってみよう
+//   #09 要素を生成してみよう
 
 {
-   const div = document.querySelector('div');
+  const h1 = document.createElement('h1');//HTMLにh1を作成
+  h1.textContent = 'Title';
+  document.body.appendChild(h1);//bodyの最後にh1を追加
 
-//    div.className = 'border-pink';//HTMLのdivのclass名がbox -> border-pinkに書き換わってしまう
-//    div.className = ' box border-pink'; // boxクラスも書いておく
-        // div.classList.add ('border-pink');
-        // div.classList.remove ('box');
+  const p = document.createElement('p');//HTMLにpを作成
+  p.textContent = 'hello.hello,hello';
+  document.body.appendChild(p);//bodyの最後にpを追加
 
-        // if(div.classList.contains ('border-pink')){
-        //     div.classList.remove ('border-pink');
-        // }else{
-        //     div.classList.add ('border-pink');
-        // }
-
-        //toggleはdivにborder-pinkが存在なし->追加、存在する->削除
-        div.classList.toggle('border-pink');
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Sub Title';
+  document.body.insertBefore(h2,p);//bodyのpの前にh2を追加
 
 }
